780c8b42df1577419328941d6f7f2458cbd6b7fb
JK00119500 <109811334+JK00119500@users.noreply.github.com>
2022-08-27 17:36:23 +0530

    Update dck-inst.sh

--- dck-inst.sh
+++ dck-inst.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-sudo yum remove docker \
-                  docker-client \
-                  docker-client-latest \
-                  docker-common \
-                  docker-latest \
-                  docker-latest-logrotate \
-                  docker-logrotate \
-                  docker-engine
-				  
-sudo yum install -y yum-utils
+sudo yum update -y
 
-sudo yum-config-manager \
-    --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
 
-sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-
-sudo yum list docker-ce --showduplicates | sort -r	
-
-sudo yum install docker-ce-<VERSION_STRING> docker-ce-cli-<VERSION_STRING> containerd.io docker-compose-plugin
+sudo yum install docker-ce --nobest -y
 
 sudo systemctl start docker
+
+sudo systemctl enable docker
+
+sudo docker --version
+
+sudo yum install curl -y
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo docker-compose --version
