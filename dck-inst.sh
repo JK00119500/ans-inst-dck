9e97beac9290db30c5e90ac4547ed7642b654f71
JK00119500 <109811334+JK00119500@users.noreply.github.com>
2022-08-29 15:09:07 +0530

    Update dck-inst.sh

--- dck-inst.sh
+++ dck-inst.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-sudo yum update -y
+sudo yum install docker -y
 
-sudo yum-config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+sudo usermod -a -G docker ec2-user
 
-sudo yum install docker-ce --nobest -y
+sudo yum install python3-pip -y
 
-sudo systemctl start docker
+sudo sudo pip3 install docker-compose -y
 
-sudo systemctl enable docker
+sudo systemctl enable docker.service
 
-sudo docker --version
-
-sudo yum install curl -y
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-sudo chmod +x /usr/local/bin/docker-compose
-
-sudo docker-compose --version
+sudo systemctl start docker.service
